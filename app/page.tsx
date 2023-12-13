5401f531f4b598eb34a1f6141febc4e021794906
= <=>
2023-12-14 00:20:23 +0800

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -7,22 +7,36 @@ import { Badge } from "@/components/ui/badge";
 export default function Home() {
   const [counter, setCounter] = useState(0);
   const [isAnimating, setIsAnimating] = useState(false);
-  const hasMounted = useRef(false);
+  const [hasMounted, setHasMounted] = useState(false);
+  const [tips, setTips] = useState<string[]>([]);
+
   const triggerMuyuHit = () => {
     setCounter((prevCounter) => {
       const res = prevCounter + 1;
       localStorage.setItem("counter", String(res));
       return res;
     });
+    setTips((prevTips) => [...prevTips, "功德+1"]);
     setIsAnimating(true);
   };
 
   useEffect(() => {
-    console.log(localStorage.getItem("counter"));
-    setCounter(()=>{
-      hasMounted.current = true;
-     return  parseInt(localStorage.getItem("counter") || "0")
-    });
+    const t = setInterval(() => {
+      setTips((tips) => {
+        tips.splice(0, 1);
+        return tips;
+      });
+    }, 500);
+
+    return () => {
+      clearInterval(t);
+    };
+  }, []);
+
+  useEffect(() => {
+    setHasMounted(true);
+    const count = parseInt(localStorage.getItem("counter") || "0");
+    setCounter(count);
   }, []);
 
   useEffect(() => {
@@ -48,25 +62,33 @@ export default function Home() {
     };
   }, [isAnimating]);
 
-
-  if(!hasMounted.current){
-    return null
+  if (!hasMounted) {
+    return null;
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24  select-none ">
       <div className="flex justify-center items-center flex-col">
         <div className="text-[96px]">{counter}</div>
         <span className=" mt-[-2rem]">功德</span>
       </div>
-      <Image
-        onClick={triggerMuyuHit}
-        src={"/muyu.svg"}
-        alt="电子木鱼"
-        width={200}
-        height={200}
-        className={cn("cursor-pointer", isAnimating && "w-f-c-i-size")}></Image>
-
+      <div>
+        <div className="flex justify-center items-center flex-col">
+          {tips.map((tip, index) => (
+            <div className="fade-out" key={index}>
+              {tip}
+            </div>
+          ))}
+        </div>
+        <Image
+          onClick={triggerMuyuHit}
+          src={"/muyu.svg"}
+          alt="电子木鱼"
+          width={200}
+          height={200}
+          className={cn("cursor-pointer", isAnimating && "w-f-c-i-size")}
+        />
+      </div>
       <footer>
         <Badge variant="outline">按下空格</Badge> /
         <Badge variant="outline">点击木鱼</Badge> 积攒功德
